7f48b36fa31cfc8bdce6142314f2dbbe6ff9b2d5
Ashish0253 <ashishrana0253@gmail.com>
2022-12-21 07:26:47 +0530

    project completed

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector("#menu");
+const menuIcon = document.querySelector("#menuIcon");
+
+menuIcon.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
